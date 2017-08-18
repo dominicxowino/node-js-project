08633744402400b439681b2aa2c6d4cc798dcbb7
Dominic Owino okelo <dominic7owino@yahoo.com>
2017-08-18 15:38:50 +0000

    Add another database

--- app.js
+++ app.js
@@ -15,11 +15,12 @@
                     
                     
                     
-                   
-             mongoose.connect('mongodb://localhost/auction');
-              // mongoose.connect('mongodb://127.0.0.1:27017/auction');
- // mongoose.connect("mongodb://<databasenameisit>:<databasenameisit>@ds149353.mlab.com:49353/databasenameisit") 
-          var url = process.env.DATABASEURL || "mongodb://localhost/auction";
+                
+         mongoose.connect('mongodb://localhost/auction');
+          
+              "mongo ds151153.mlab.com:51153/auction -u <dominic> -p <databasepassword>";
+  //mongoose.connect("mongodb://<dominic>:<databasepassword>@ds151153.mlab.com:51153/auction") 
+    var url = process.env.DATABASEURL || "mongodb://localhost/auction";
                 
                 app.use(bodyParser.urlencoded({extended: true}));
                 app.set("view engine", "ejs");
