cfeed1dc278a35ed288f2093f50a8a32dd8793ca
Dominic Owino okelo <dominic7owino@yahoo.com>
2017-08-18 14:52:02 +0000

    Add all

---
+++ README.md
@@ -0,0 +1,4 @@
+#This is an open source project built with NodeJS,MongoDB and Express framework.It is an auction platform that implements all a production appplication should be able to do/achieve.Hosted on Heroku.com and db provided by mlab.com
+EJS templating engine is used to serve the templates.
+User sign-up,log-in,admin,authentication and authorization is implemented using passport.
+Feel free to fork and use the codes for your own good.
--- app.js
+++ app.js
@@ -16,10 +16,10 @@
                     
                     
                    
-                  mongoose.connect('mongodb://localhost/auction');
+             mongoose.connect('mongodb://localhost/auction');
               // mongoose.connect('mongodb://127.0.0.1:27017/auction');
-    // mongoose.connect("mongodb://<databasenameisit>:<databasenameisit>@ds149353.mlab.com:49353/databasenameisit") 
-           var url = process.env.DATABASEURL || "mongodb://localhost/auction";
+ // mongoose.connect("mongodb://<databasenameisit>:<databasenameisit>@ds149353.mlab.com:49353/databasenameisit") 
+          var url = process.env.DATABASEURL || "mongodb://localhost/auction";
                 
                 app.use(bodyParser.urlencoded({extended: true}));
                 app.set("view engine", "ejs");
